d5061afebcbeda0b32023815fd64aa8a5c926d41
FlightlessMango <flightlessmangoyt@gmail.com>
2023-04-18 00:17:11 +0200

    hud elements: exec_name: push the name one column over

--- src/hud_elements.cpp
+++ src/hud_elements.cpp
@@ -1126,6 +1126,7 @@ void HudElements::exec_name(){
         ImGui::TableNextColumn();
         ImGui::TextColored(HUDElements.colors.engine, "%s", "Executable name");
         ImGui::TableNextColumn();
+        ImGui::TableNextColumn();
         right_aligned_text(HUDElements.colors.text, HUDElements.ralign_width, global_proc_name.c_str());
         ImGui::PopFont();
     }
